2e2b8cb4eb085c52b93247ad7137dfd5e06727f6
Pablo Marino <mentor.rolling@gmail.com>
2024-10-02 12:47:15 -0300

    Update routes

--- src/routes/RoutesSec.jsx
+++ src/routes/RoutesSec.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import { getUsuario } from "../helpers/apiUsuarios";
 import HomeScreen from "../views/HomeScreen";
 import NotFoundScreen from "../views/NotFoundScreen";
 import ProductoScreen from "../views/ProductoScreen";
