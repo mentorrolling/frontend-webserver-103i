1eb1596eccf24eafeab710c1ab6b6b07c5325af3
Pablo Marino <mentor.rolling@gmail.com>
2024-10-04 14:25:58 -0300

    Agrego comentarios a las funciones apiUsuarios y fetchApi

--- src/helpers/apiUsuarios.js
+++ src/helpers/apiUsuarios.js
@@ -1,17 +1,18 @@
-const url = "http://localhost:3000/api/usuarios";
+const url = "http://localhost:3000/api/usuarios"; //Definimos la url de la API
 
 const getUsuario = async (id) => {
   const resp = await fetch(url + "/" + id, {
-    method: "GET",
+    //petición fetch a la API con la ruta como definimos en el backend
+    method: "GET", //indicamos el método
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
-      "x-token": JSON.parse(localStorage.getItem("token")),
+      "Content-type": "application/json; charset=UTF-8", //header básico
+      "x-token": JSON.parse(localStorage.getItem("token")), // información del token que guardamos en localstorage
     },
   });
 
-  const data = await resp.json();
+  const data = await resp.json(); //recibimos los datos obtenidos en formato json
 
-  return data;
+  return data; //retornamos los datos obtenidos
 };
 
-export { getUsuario };
+export { getUsuario }; //exportamos la función
--- src/helpers/fetchApi.js
+++ src/helpers/fetchApi.js
@@ -1,17 +1,20 @@
-const url = "http://localhost:3000/api/";
+const url = "http://localhost:3000/api/"; //Definimos la url de la API
+
+// const url ="https://webserver-103i.onrender.com/api/"
 
 const auth = async (email, password) => {
   const resp = await fetch(url + "auth/login", {
-    method: "POST",
-    body: JSON.stringify({ email, password }),
+    //petición fetch a la API con la ruta como definimos en el backend
+    method: "POST", //indicamos el método
+    body: JSON.stringify({ email, password }), //En body enviamos los datos de correo y password como objeto
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      "Content-type": "application/json; charset=UTF-8", //header básico
     },
   });
 
-  const data = await resp.json();
+  const data = await resp.json(); //recibimos los datos obtenidos en formato json
 
-  return data;
+  return data; //retornamos los datos obtenidos
 };
 
-export { auth };
+export { auth }; //exportamos la función
